7f023abd28ddfc90ee42bc7cbe1a083df5da9b2d
ruhul2003 <ruhul941020@gmail.com>
2026-04-25 00:02:33 +0600

    Added navbar

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        className="dark:invert"
-        width={100}
-        height={24}
-        priority
-      />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      
+      
     </div>
   );
 }
---
+++ src/app/posts/page.jsx
@@ -0,0 +1,11 @@
+
+
+const PostPage = () => {
+    return (
+        <div>
+            <h1>Posts</h1>
+        </div>
+    );
+};
+
+export default PostPage;
